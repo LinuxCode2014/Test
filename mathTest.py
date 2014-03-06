279cd5828c511f532627136791a3c593e2dd7c69
LinuxCode2014 <Suv121@yandex.ru>
2014-03-06 18:45:48 +0400

    test

---
+++ mathTest.py
@@ -0,0 +1,58 @@
+import math
+from random import randrange, getrandbits
+from itertools import repeat
+from functools import reduce
+
+def getPrime(n):
+    """Get a n-bit pseudo-random prime"""
+    def isProbablePrime(n, t = 7):
+        """Miller-Rabin primality test"""
+        def isComposite(a):
+            """Check if n is composite"""
+            if pow(a, d, n) == 1:
+                return False
+            for i in range(s):
+                if pow(a, 2 ** i * d, n) == n - 1:
+                    return False
+            return True
+
+        assert n > 0
+        if n < 3:
+            return [False, False, True][n]
+        elif not n & 1:
+            return False
+        else:
+            s, d = 0, n - 1
+            while not d & 1:
+                s += 1
+                d >>= 1
+        for _ in repeat(None, t):
+            if isComposite(randrange(2, n)):
+                return False
+        return True
+
+    p = getrandbits(n)
+    while not isProbablePrime(p):
+        p = getrandbits(n)
+    return p
+
+
+
+def data():
+    hash = "202db79132e60f90671face316f41b39"
+    closeKey = 25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784406918290641249515082189298559149176184502808489120072844992687392807287776735971418347270261896375014971824691165077613379859095700097330459748808428401797429100642458691817195118746121515172654632282216869987549182422433637259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133844143603833904414952634432190114657544454178424020924616515723350778707749817125772467962926386356373289912154831438167899885040445364023527381951378636564391212010397122822120720357
+    print ("CloseKey = ", closeKey)
+    U = getPrime(1024)
+    r = getPrime(1024)
+    q = getPrime(1024)
+    n = r*q
+
+    print ("U = ", U)
+    print ("n = ", n)
+
+    #Z = (int(hash)**(-1))*
+
+
+if __name__ == "__main__":
+    data()
+
